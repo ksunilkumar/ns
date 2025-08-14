2c17edf08e2640bb97327aa441c4120c1e9f8e48
skroy308 <skroy308@gmail.com>
2025-08-14 17:40:19 +0530

    Initial commit

---
+++ src/main/java/com/nsind/ns/controller/package-info.java
@@ -0,0 +1,1 @@
+package com.nsind.ns.controller;
---
+++ src/test/java/com/nsind/ns/NsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsind.ns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
